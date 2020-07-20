ab16cf8f7f8ff1fe098a97d213695ef7e320ffde
awende <alex.wende@sparkfun.com>
2020-07-20 15:07:50 -0600

    Updated library
    - Update the library name
    - Added comments to example

--- examples/Example1_BasicReadings/Example1_BasicReadings.ino
+++ examples/Example1_BasicReadings/Example1_BasicReadings.ino
@@ -11,20 +11,49 @@
 */
 
 // Include the SparkFun MicroPressure library.
-// Click here to get the library: http://librarymanager/All#SparkFun_MPR
+// Click here to get the library: http://librarymanager/All#SparkFun_MicroPressure
 
 #include<Wire.h>
-#include "SparkFun_MPR.h"
+#include <SparkFun_MicroPressure.h>
 
-SparkFun_MPR mpr;
+/*
+ * Initialize Constructor
+ * Optional parameters:
+ *  - EOC_PIN: End Of Conversion (defualt: -1)
+ *  - RST_PIN: Reset (defualt: -1)
+ *  - MIN_PSI: Minimum Pressure (default: 0 PSI)
+ *  - MAX_PSI: Maximum Pressure (default: 25 PSI)
+ */
+//SparkFun_MicroPressure mpr(EOC_PIN, RST_PIN, MIN_PSI, MAX_PSI);
+SparkFun_MicroPressure mpr; // Use default values with reset and EOC pins unused
 
 void setup() {
+  // Initalize UART, I2C bus, and connect to the micropressure sensor
   Serial.begin(115200);
   Wire.begin();
-  mpr.begin();
+
+  /* The micropressure sensor uses default settings with the address 0x18 using Wire.
+
+     The mircropressure sensor has a fixed I2C address, if another address is used it
+     can be defined here. If you need to use two micropressure sensors, and your
+     microcontroller has multiple I2C buses, these parameters can be changed here.
+
+     E.g. mpr.begin(ADDRESS, Wire1)
+
+     Will return true on success or false on failure to communicate. */
+  if(!mpr.begin())
+  {
+    Serial.println("Cannot connect to MicroPressure sensor.");
+    while(1);
+  }
 }
 
 void loop() {
+  /* The micropressure sensor outputs pressure readings in pounds per square inch (PSI).
+     Optionally, if you prefer pressure in another unit, the library can convert the
+     pressure reading to: pascals, kilopascals, bar, torr, inches of murcury, and
+     atmospheres.
+   */
   Serial.print(mpr.readPressure(),4);
   Serial.println(" PSI");
   Serial.print(mpr.readPressure(PA),1);
